ae02956abd82c9c55a0835a4995ee3db4b5e5ddb
Ryan Bales <ryankbales@gmail.com>
2015-01-19 13:36:48 -0800

    adds link helpers and datetime sanitizer

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,15 @@
 module ApplicationHelper
+  def date_time_sanitizer(obj)
+    if obj
+      obj = obj.strftime("%A" + ", " "%b %d" + " at " "%l:%M%P %Y")
+    end
+  end
+
+  def normalize_external_link(label, link)
+    if link.include?('http://') || link.include?('https://')
+      link = link_to(label, link, target: 'blank')
+    else
+      link = link_to(label, "http://#{link}", target: 'blank')
+    end
+  end
 end
