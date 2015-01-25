c9ba35ddae5f5a5ab8fa7f8d2eb70fa60b02e37d
Ryan Bales <ryankbales@gmail.com>
2015-01-25 10:48:08 -0800

    setting up user edit and show pages

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,12 @@
 class UsersController < ApplicationController
+  before_action :set_user, only: [:show, :edit, :update]
+
   def new
     @user = User.new
+  end
+
+  def show
+
   end
 
   def create
@@ -14,7 +20,25 @@ class UsersController < ApplicationController
     end
   end
 
+  def edit
+  end
+
+  def update
+    if @user.update(user_params)
+      flash[:notice] = "Your Was Updated"
+      redirect_to user_path
+    else
+      render :new
+    end
+  end
+
   def user_params
     params.require(:user).permit(:username, :password)
+  end
+
+  private
+
+  def set_user
+    @user = User.find(params[:id])
   end
 end
