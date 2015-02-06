965f3d057c5eb4340f77c6fc3b43854c0ad61d46
Ryan Bales <ryankbales@gmail.com>
2015-02-06 15:41:27 -0800

    better slugs

--- app/models/user.rb
+++ app/models/user.rb
@@ -11,7 +11,30 @@ class User < ActiveRecord::Base
   before_save :generate_slug
 
   def generate_slug
-    self.slug = self.username.gsub(" ", "-").downcase
+    the_slug = to_slug(self.username)
+    user = User.find_by slug: the_slug
+    count = 2
+    while user && user != self
+      the_slug = append_suffix(the_slug, count)
+      post = User.find_by slug: the_slug
+      count += 1
+    end
+    self.slug = the_slug.downcase
+  end
+
+  def append_suffix(str, count)
+    if str.split('-').last.to_i != 0
+      return str.split('-').slice(0...-1).join('-') + "-" + count.to_s
+    else
+      return str + "-" + count.to_s
+    end
+  end
+
+  def to_slug(name)
+    str = name.strip
+    str.gsub!(/\s*[^A-Za-z0-9]\s*/, '-')
+    str.gsub!(/-+/, "-")
+    str.downcase
   end
 
   def to_param
