c3a496e476165109e6d303c85bea77dfbb0a9749
Ryan Bales <ryankbales@gmail.com>
2015-02-17 08:02:29 -0800

    not sure what this change is

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,6 +1,7 @@
 class PostsController < ApplicationController
   before_action :set_post, only: [:show, :edit, :update, :vote]
   before_action :require_user, except: [:index, :show]
+  before_action :require_admin, only: [:edit, :update]
 
   def index
     @posts = Post.all.sort_by{|x| x.total_votes}.reverse
